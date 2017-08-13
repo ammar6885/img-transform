273bb796f529fba2ca59862644425ee7e5285b3f
Ammar <amrmrdb1@gmail.com>
2017-08-13 13:31:21 +0300

    v1.0.0

---
+++ README.md
@@ -0,0 +1,100 @@
+Resize and crop uploaded images on the client side.
+
+## Installation
+
+```bash
+$ npm install img-transform
+```
+
+## Usage
+
+```js
+imgTransform(uploadInput.files[0], 'image/jpeg')
+  .done(function (dataUrl) {
+    // your code here
+  });
+```
+
+### Resize
+
+```js
+imgTransform(uploadInput.files[0])
+  .resize(560, 340, 'cover')
+  .done(function (dataUrl) {
+    // your code here
+  });
+```
+
+#### Options
+
+  **width** new width in pixels or 'auto'.
+  **height** new height in pixels or 'auto'.
+  **mode** _optional_ - preserve image ratio with two options 'cover' | 'contain'
+
+  _note_: if one of dimensions set to 'auto' the other dimension must be set to pixel value.
+
+### Crop
+
+```js
+imgTransform(uploadInput.files[0])
+  .crop(560, 340, 10, 10)
+  .done(function (dataUrl) {
+    // your code here
+  });
+```
+
+#### Options
+  **width** crop width in pixels or 'auto'.
+  **height** crop height in pixels or 'auto'.
+  **leftOffset** crop left offset in pixels or 'auto'.
+  **topOffset** crop top offset in pixels or 'auto'.
+
+  Dimensions 'auto' options to preserve the image ratio.
+  Offsets 'auto' options to center the crop size in related dimension.
+
+### Serial
+```js
+imgTransform(uploadInput.files[0])
+  .crop(560, 340, 10, 10)
+  .resize(560, 340, 'cover')
+  .done(function (dataUrl) {
+    // your code here
+  });
+```
+
+You have the option to stack the two previous methods as in the example above.
+
+## auto
+```js
+imgTransform(uploadInput.files[0])
+  .auto(560, 340)
+  .done(function (dataUrl) {
+    // your code here
+  });
+```
+
+### Options
+
+  **width** new width and crop width in pixels or 'auto'.
+  **height** new height and crop height in pixels or 'auto'.
+
+The 'auto' method with perform the 'crop' and 'resize' operations togather with the both offsets set to 'auto' and mode set to 'cover'.
+
+## done
+```js
+imgTransform(uploadInput.files[0])
+  .auto(560, 340)
+  .done(function (dataUrl) {
+    // your code here
+  });
+```
+
+### options
+
+  **callback** callback function with the result image in 64string from as the first argument
+
+
+
+
+
+Thank you.
---
+++ index.js
@@ -0,0 +1,167 @@
+(function (global, factory) {
+  if (typeof define === 'function' && define.amd) { // AMD
+    define([], function () { return factory(); });
+  } else if (typeof module !== 'undefined' && typeof exports === 'object') { // Node.js
+    module.exports = factory();
+  } else if (global !== undefined) { // Global variable
+    global.imgTransform = factory();
+  }
+})(this || window, function () {
+
+  var Mod = {},
+    result, imgType,
+    success, resizeImg, cropImg,
+    resizeWidth, resizeHeight, mode,
+    cropWidth, cropHeight, cropLeft, cropTop;
+
+  function _createImg(src, callback) {
+    var img = new Image();
+    img.onload = callback.bind(img);
+    img.src = src;
+  }
+
+  function _crop() {
+    _createImg(result, function () {
+      var ratio = this.width / this.height,
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        marginLeft = cropLeft,
+        marginTop = cropTop;
+
+      if (cropWidth === 'auto' && cropHeight === 'auto') {
+        cropWidth = this.width;
+        cropHeight = this.height;
+      } else if (cropWidth === 'auto') {
+        cropWidth = ratio * cropHeight;
+      } else if (cropHeight === 'auto') {
+        cropHeight = cropWidth / ratio;
+      }
+
+      cropWidth = cropWidth > this.width ? this.width : cropWidth;
+      cropHeight = cropHeight > this.height ? this.height : cropHeight;
+
+      if (marginLeft === 'auto') marginLeft = (this.width - cropWidth) / 2;
+      if (marginTop === 'auto') marginTop = (this.height - cropHeight) / 2;
+
+      canvas.width = cropWidth;
+      canvas.height = cropHeight;
+
+      ctx.drawImage(this, marginLeft, marginTop, cropWidth, cropHeight, 0, 0, cropWidth, cropHeight);
+      result = canvas.toDataURL(imgType, 1);
+
+      if (resizeImg && cropImg < resizeImg) _resize();
+      else if (success) success(result);
+    });
+  }
+
+  function _resize() {
+    _createImg(result, function () {
+      var ratio = this.width / this.height,
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        scaleToWidth;
+
+      if (resizeWidth === 'auto' && resizeHeight === 'auto') {
+        resizeWidth = this.width;
+        resizeHeight = this.height;
+      } else if (resizeWidth === 'auto') {
+        resizeWidth = ratio * resizeHeight;
+      } else if (resizeHeight === 'auto') {
+        resizeHeight = resizeWidth / ratio;
+      } else if (mode) {
+        scaleToWidth = ratio * resizeHeight < resizeWidth;
+
+        if ((scaleToWidth && mode === 'cover') || (!scaleToWidth && mode === 'contain')) resizeHeight = resizeWidth / ratio;
+        else resizeWidth = ratio * resizeHeight;
+      }
+
+      this.width = resizeWidth;
+      this.height = resizeHeight;
+
+      canvas.width = resizeWidth;
+      canvas.height = resizeHeight;
+
+      ctx.drawImage(this, 0, 0, resizeWidth, resizeHeight);
+      result = canvas.toDataURL(imgType, 1);
+
+      if (cropImg && resizeImg < cropImg) _crop();
+      else if (success) success(result);
+    });
+  }
+
+  /**
+   * Grap image file or image 64string
+   * @param {Object} file 
+   * @param {String} type 
+   */
+  function imgTransform(file, type) {
+    result = undefined;
+    imgType = undefined;
+    success = undefined;
+    resizeImg = undefined;
+    cropImg = undefined;
+    resizeWidth = undefined;
+    resizeHeight = undefined;
+    mode = undefined;
+    cropWidth = undefined;
+    cropHeight = undefined;
+    cropHeight = undefined;
+    cropTop = undefined;
+
+    if (typeof file === 'string') {
+      result = file;
+      imgType = type || 'image/jpeg';
+      setTimeout(function () { next(); }, 5);
+    } else {
+      var reader = new FileReader();
+      imgType = type || file.type;
+      reader.onload = function (e) {
+        result = e.target.result;
+        next();
+      }
+
+      reader.readAsDataURL(file);
+    }
+
+    function next() {
+      if (resizeImg && cropImg)
+        if (resizeImg < cropImg) _resize();
+        else _crop();
+      else if (resizeImg) _resize();
+      else if (cropImg) _crop();
+      else if (success) success(result);
+    }
+
+    return Mod;
+  }
+
+  Mod.crop = function (w, h, x, y) {
+    cropWidth = w || 'auto';
+    cropHeight = h || 'auto';
+    cropLeft = x || 'auto';
+    cropTop = y || 'auto';
+    cropImg = resizeImg ? 2 : 1;
+
+    return Mod;
+  };
+
+  Mod.resize = function (w, h, resizeMode) {
+    resizeWidth = w || 'auto';
+    resizeHeight = h || 'auto';
+    mode = resizeMode || null;
+    resizeImg = cropImg ? 2 : 1;
+    return Mod;
+  };
+
+  Mod.auto = function (w, h) {
+    return Mod
+      .resize(w, h, 'cover')
+      .crop(w, h);
+  };
+
+  Mod.done = function (callback) {
+    success = callback;
+  };
+
+  return imgTransform;
+});
